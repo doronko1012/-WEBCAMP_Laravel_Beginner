6a243b84c3ba75239b2248c09888ecd25728e946
EC2 Default User <ec2-user@ip-172-31-36-181.ap-northeast-1.compute.internal>
2024-12-02 07:38:46 +0000

    サンプルページのURLを変更した

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ use App\Http\Controllers\WelcomeController;
 //     return view('welcome');
 // });
 
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/welcome', [WelcomeController::class, 'index']);
 
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
