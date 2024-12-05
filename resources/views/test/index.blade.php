c48cc722be71091b2ae5e481a01ad571b5cd0985
EC2 Default User <ec2-user@ip-172-31-36-181.ap-northeast-1.compute.internal>
2024-12-05 12:28:22 +0000

    認証の「formでーあを受け取る」所まで書いた

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,19 +1,20 @@
-@extends('layout')
+@extends('test.layout')
 
 {{--メインコンテンツ --}}
 @section('contents')
-        <h1>ログイン</h1>
-        @if ($errors ->any())
+    
+        @if ($errors->any())
             <div>
             @foreach ($errors->all() as $error)
                 {{ $error }}<br>
             @endforeach
             </div>
         @endif
-        <form action="/login" method="post">
+        
+        <form action="/test/input" method="post">
             @csrf
             email：<input type="text" name="email" value="{{ old('email') }}"><br>
             パスワード：<input type="password" name="password"><br>
-            <button>ログインする</button>
+            <button>送信する</button>
         </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,14 @@ use App\Http\Controllers\TaskController;
 */
 // タスク管理システム
 Route::get('/', [AuthController::class, 'index']);
+Route::get('/task/list', [TaskController::class, 'list']);
+Route::Post('/login', [AuthController::class, 'login']);
+
 
 // テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
-Route::get('/task/list', [TaskController::class, 'list']);
+
+// formテスト用
+Route::get('/test', [TestController::class, 'index']);
+Route::post('/test/input', [TestController::class, 'input']);
